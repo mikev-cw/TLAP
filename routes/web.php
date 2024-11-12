3c65b2ed22ca54864eb8b5f706bac8848796c411
Mike V <mike@cyway.it>
2024-11-12 21:57:37 +0100

    breeze installation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Trackables;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/trackables/createNew', [Trackables::class, 'createNew']);
-Route::get('/trackables/{uid}', [Trackables::class, 'showByUid']);
-Route::get('/trackables', [Trackables::class, 'directory']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/trackables/createNew', [Trackables::class, 'createNew'])->middleware(['auth']);;
+Route::get('/trackables/{uid}', [Trackables::class, 'showByUid'])->middleware(['auth']);;
+Route::get('/trackables', [Trackables::class, 'directory'])->middleware(['auth']);;
+
+require __DIR__.'/auth.php';
