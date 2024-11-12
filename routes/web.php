da23109498efdefc25f72bac80d94c2a4fdf4ec7
Mike V <mike@cyway.it>
2024-11-12 23:27:05 +0100

    sanctum installation

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/trackables/createNew', [Trackables::class, 'createNew']);
+    Route::get('/trackables/{uid}', [Trackables::class, 'showByUid']);
+    Route::get('/trackables', [Trackables::class, 'directory']);
 });
 
-Route::get('/trackables/createNew', [Trackables::class, 'createNew'])->middleware(['auth']);;
-Route::get('/trackables/{uid}', [Trackables::class, 'showByUid'])->middleware(['auth']);;
-Route::get('/trackables', [Trackables::class, 'directory'])->middleware(['auth']);;
 
 require __DIR__.'/auth.php';
